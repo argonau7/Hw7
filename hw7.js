7dd4d67f6cdbf8a079b2c2d9a9442e9d1cf45842
Lukas <lukas.zuegg@gmail.com>
2020-04-23 23:27:23 +0200

    first commit

---
+++ hw7.js
@@ -0,0 +1,17 @@
+
+firstarray = [1, 4, -1, 5, 18, -16]
+
+function sumarray() {
+var sum=0
+for (let i=0;i<firstarray.length;i++){
+   console.log(sum+=firstarray[i])  //how to only show final result?
+   
+}
+
+}
+
+function minarray() {
+console.log(Math.min(...firstarray))
+
+}
+
